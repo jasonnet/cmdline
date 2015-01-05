111c302032817db5e31c0679140e91d78edc901b
jasonnet <nnd110423githb@smallcue.com>
2015-01-05 22:02:49 +0000

    trying to improve formating of the javadoc

--- src/main/java/org/jasonnet/cmdline/CommandLine.java
+++ src/main/java/org/jasonnet/cmdline/CommandLine.java
@@ -10,10 +10,10 @@ import java.util.LinkedList;
  * <pre>
  * Syntaxes:
  *
- * <code>---flag </code> a named flag
- * <code>---xx... </code> a commented out simple named flag
- * <code>--key value </code> a key value pair
- * <code>--xxkey value</code> a commented out key value pair
+ * <code>---flag </code>        a named flag 
+ * <code>---xx... </code>       a commented out named flag 
+ * <code>--key value </code>    a key value pair 
+ * <code>--xxkey value</code>   a commented out key value pair 
  * <code>-a</code> (or <code>-abc</code>) single-letter flag(s)
  * </pre>
  *
